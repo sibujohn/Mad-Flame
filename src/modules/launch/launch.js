44b790a5f296d8a9dee27da2fdec1b569ba754cc
Sibu John <sibu.john@inapp.com>
2017-08-24 17:02:46 +0530

    map pinning

--- src/modules/launch/launch.js
+++ src/modules/launch/launch.js
@@ -2,8 +2,8 @@ var loginApp = angular.module("launchApp", []);
 
 loginApp.controller('launchCtrl', ['$scope', '$rootScope', 'launchService',
     function($scope, $rootScope, launchService ){
-    	var map;
-        var searchBox;
+    	$scope.map;
+        $scope.searchBox;
         $scope.initMapScope = function(){
             setTimeout(function(){
                 initMap();
@@ -11,33 +11,44 @@ loginApp.controller('launchCtrl', ['$scope', '$rootScope', 'launchService',
         };
         function initMap(){
             if(typeof google !== 'undefined' && typeof google === 'object' && google.maps && google.maps.Map){
-                map = new google.maps.Map(document.getElementById('map'), {
+                $scope.map = new google.maps.Map(document.getElementById('map'), {
                     minZoom: 2,
                     zoom: 5,
                     center: {lat: 40, lng: -90},
                     scaleControl: false,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 });
-                var input = document.getElementById('place-search');
-                searchBox = new google.maps.places.SearchBox(input);
-                map.addListener('bounds_changed', function() {
-                    searchBox.setBounds(map.getBounds());
-                });
-                searchBox.addListener('places_changed', function() {
-                    palceSearched();
-                });
+                if(typeof $scope.map === 'object'){
+                    bindIdleEventOfMap();
+                    initSearchBoxFunction();
+                }
             }
         };
-        function palceSearched(){
-            var places = searchBox.getPlaces();
+        function initSearchBoxFunction(){
+            var input = document.getElementById('place-search');
+            $scope.searchBox = new google.maps.places.SearchBox(input);
+            $scope.map.addListener('bounds_changed', function() {
+                $scope.searchBox.setBounds($scope.map.getBounds());
+            });
+            $scope.searchBox.addListener('places_changed', function() {
+                placeSearched();
+            });
+        };
+        function bindIdleEventOfMap () {
+            google.maps.event.addListener($scope.map, 'idle', function() {
+                sendMapBounds();
+            });
+        };
+        function placeSearched(){
+            var places = $scope.searchBox.getPlaces();
             if (places.length == 0) {
                 return;
             }
             var bounds = new google.maps.LatLngBounds();
             places.forEach(function(place) {
                 if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
+                    console.log("Returned place contains no geometry");
+                    return;
                 }
                 if (place.geometry.viewport) {
                 // Only geocodes have viewport.
@@ -46,10 +57,22 @@ loginApp.controller('launchCtrl', ['$scope', '$rootScope', 'launchService',
                     bounds.extend(place.geometry.location);
                 }
             });
-            map.fitBounds(bounds);
-            getPins(bounds);
-        }
-        function getPins(boundary){
+            $scope.map.fitBounds(bounds);
+        };
+        function sendMapBounds(){
+            var mapBounds = $scope.map.getBounds();
+            var bounds = {
+                MinLat:mapBounds.getSouthWest().lat(),
+                MaxLat:mapBounds.getNorthEast().lat(),
+                MinLng:mapBounds.getSouthWest().lng(),
+                MaxLng:mapBounds.getNorthEast().lng()
+            }
+            launchService.sendMapBounds(bounds)
+            .then(function(){
+                getPins();
+            });
+        };
+        function getPins(){
             launchService.getPins().then(function(data){
                 for(var i=0; i<data.pins.length; i++){
                     var marker = new google.maps.Marker({
@@ -58,10 +81,10 @@ loginApp.controller('launchCtrl', ['$scope', '$rootScope', 'launchService',
                             lng : data.pins[i].lng
                         },
                         title: data.pins[i].description,
-                        map:map
+                        map:$scope.map
                     });
                 }
-            });            
+            });
         };
     }
 ]);
@@ -72,14 +95,19 @@ loginApp.service('launchService', ['factoryService', '$q',
         var serviceObject = {};
         serviceObject.serviceUrl = "launch/";
 
+        serviceObject.sendMapBounds = function(data){
+            var def = $q.defer();
+            factoryService.ajaxRequests(serviceObject.serviceUrl+"sendBounds ", "POST", data).then(function(response) {
+                def.resolve(response.data);
+            });            
+            return def.promise;
+        };
         serviceObject.getPins = function(data){
-
             var def = $q.defer();
             factoryService.ajaxRequests(serviceObject.serviceUrl+"getPins", "GET", data).then(function(response) {
                 def.resolve(response.data);
             });            
             return def.promise;
-
         };
         return serviceObject;
     }
